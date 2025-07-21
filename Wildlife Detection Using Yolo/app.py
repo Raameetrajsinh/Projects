e59f8bf8a8553917c90b693c7c9ba6b0698bfbf2
Meetrajsinh Chudasama <163631139+Raameetrajsinh@users.noreply.github.com>
2025-07-21 15:49:09 +0530

    Add files via upload

---
+++ Wildlife Detection Using Yolo/app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import tempfile
+import cv2
+from ultralytics import YOLO
+import os
+
+@st.cache_resource
+def load_model():
+    return YOLO("best.pt") 
+
+model = load_model()
+
+st.title("Wildlife Detection")
+
+uploaded_video = st.file_uploader("Upload a video", type=["mp4", "mov", "avi", "mkv"])
+
+if uploaded_video is not None:
+    input_temp = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4")
+    input_temp.write(uploaded_video.read())
+
+
+    cap = cv2.VideoCapture(input_temp.name)
+
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_interval = max(int(fps // 5), 1)  
+
+    output_path = os.path.join(tempfile.gettempdir(), "output_annotated.mp4")
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+
+    stframe = st.empty()
+    frame_count = 0
+ 
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        if frame_count % frame_interval == 0:
+            results = model.predict(source=frame, conf=0.3, imgsz=640)
+            annotated = results[0].plot()
+            out.write(annotated)  
+            stframe.image(annotated, channels="BGR", use_container_width=True)
+        else:
+            out.write(frame)  
+
+        frame_count += 1
+
+    cap.release()
+    out.release()
+
+    st.success("Done processing video.")
+
+ 
